d3f063d0ac52e27a134ae24d36a9939ddf74c0a3
cmelion <cfulnecky@gmail.com>
2025-05-17 00:21:42 -0400

    Fix vercel deployment

--- vite.config.ts
+++ vite.config.ts
@@ -3,21 +3,16 @@ import react from "@vitejs/plugin-react-swc";
 import path from "path";
 import { componentTagger } from "lovable-tagger";
 
+// https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: mode === "production" ? "/token-trio-calculator/" : "/", // Adjust for GitHub Pages
   server: {
     host: "::",
     port: 8080,
-    hmr: {
-      overlay: true,
-    },
-    watch: {
-      usePolling: true,
-    },
   },
   plugins: [
     react(),
-    mode === "development" && componentTagger(),
+    mode === 'development' &&
+    componentTagger(),
   ].filter(Boolean),
   resolve: {
     alias: {
